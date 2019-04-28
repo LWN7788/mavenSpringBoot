31b9fc79a4d01de3dbb56d98686fa0a8be88c926
ljy <11>
2019-04-28 22:21:40 +0800

    28wan

--- src/main/java/com/sunvua/ljy/controller/UserController.java
+++ src/main/java/com/sunvua/ljy/controller/UserController.java
@@ -15,13 +15,31 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 
 @Controller
-@RequestMapping("/user")
+@RequestMapping(value = "/user",method = {RequestMethod.POST} )
 public class UserController extends BaseController{
     @Autowired
     UserService userService;
+    @Autowired
+    HttpServletRequest httpServletRequest;
+    @RequestMapping("/getotp")
+    @ResponseBody
+    @CrossOrigin//解决跨域
+    public ReturnCommonType getOtp(@RequestParam(name="telphone")String telphone){
+        Random random=new Random();
+        int randomInt=random.nextInt(99999);
+        randomInt+=10000;
+        String otpCode=String.valueOf(randomInt);
+        httpServletRequest.getSession().setAttribute(telphone,otpCode);
+        System.out.println("telphone="+telphone+"&otpcode="+otpCode);
+        return ReturnCommonType.create(null);
+    }
+
+
+
     @RequestMapping("/get")
     @ResponseBody
     public ReturnCommonType getUser(@RequestParam(name="id") Integer id) throws BusinessExcepiton{
